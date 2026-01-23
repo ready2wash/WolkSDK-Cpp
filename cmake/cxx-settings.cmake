bd2172ca6b41d515bc517e781bdf71ebc999896c
milanzlicic <milan.zlicic@ready2wash.com>
2026-01-23 16:32:11 +0100

    per target compiler flags

--- cmake/cxx-settings.cmake
+++ cmake/cxx-settings.cmake
@@ -1,21 +1,14 @@
 # Set the standard and flags
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}     -Wall -Wextra -pedantic                                                    \
-                                            -pedantic-errors -Wcast-align                                              \
-                                            -Wcast-qual -Wconversion                                                   \
-                                            -Wdisabled-optimization                                                    \
-                                            -Wfloat-equal -Wformat=2                                                   \
-                                            -Werror=init-self                                                          \
-                                            -Werror=missing-field-initializers -Wmissing-format-attribute              \
-                                            -Wmissing-noreturn -Werror=pointer-arith                                   \
-                                            -Wno-packed  -Wno-padded -Wredundant-decls                                 \
-                                            -Werror=shadow -Werror=stack-protector                                     \
-                                            -Wstrict-aliasing=2 -Wno-switch                                            \
-                                            -Werror=unreachable-code -Wunused                                          \
-                                            -Wvariadic-macros                                                          \
-                                            -Wwrite-strings -Werror=non-virtual-dtor -Werror=return-type")
+set(WOLKSDK_CXX_STANDARD cxx_std_17)
+set(WOLKSDK_CXX_FLAGS -Wall -Wextra -pedantic -pedantic-errors -Wcast-align
+    -Wcast-qual -Wconversion -Wdisabled-optimization -Wfloat-equal -Wformat=2
+    -Werror=init-self -Werror=missing-field-initializers -Wmissing-format-attribute
+    -Wmissing-noreturn -Werror=pointer-arith -Wno-packed  -Wno-padded -Wredundant-decls
+    -Werror=shadow -Werror=stack-protector -Wstrict-aliasing=2 -Wno-switch
+    -Werror=unreachable-code -Wunused -Wvariadic-macros -Wwrite-strings
+    -Werror=non-virtual-dtor -Werror=return-type)
 
 # Set the tests flags
 if (WOLKSDK_BUILD_TESTS)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage -g")
+    set(WOLKSDK_CXX_FLAGS "${WOLKSDK_CXX_FLAGS} -fprofile-arcs -ftest-coverage -g")
 endif ()
